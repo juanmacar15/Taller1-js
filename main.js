7a83ef5a7f5f61cd4412e73849e361250a643742
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-15 20:08:56 -0500

    ejercicio 3

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let n1 = parseInt(prompt('Ingrese el lado 1'));
-let n2 = parseInt(prompt('Ingrese el lado 2'));
+alert('hallar la media de 4 numeros')
+let n1 = parseInt(prompt('Ingrese el primer numero'));
+let n2 = parseInt(prompt('Ingrese el segundo numero'));
+let n3 = parseInt(prompt('Ingrese el tercer numero'));
+let n4 = parseInt(prompt('Ingrese el cuarto numero'));
 
-let rect = n1 * n2;
+let media = (n1 + n2 + n3 + n4) / 4;
 
 
-document.getElementById('rect').innerHTML = rect;
+document.getElementById('media').innerHTML = media;
 
 
