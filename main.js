044028936582e136c3bdeb5590fc5b0fa53e9ca5
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-15 20:27:16 -0500

    ejercicio 5

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-alert('hallar el area de un triangulo')
-let base = parseInt(prompt('Ingrese la base'));
-let alt = parseInt(prompt('Ingrese la altura'));
+alert('hallar la longitud y el area de un circulo')
+
+const pi = 3.1416;
+
+let radio = parseInt(prompt('Ingrese el radio del circulo'));
 
 
-let area = (base * alt) / 2;
+let long = 2 * pi * radio;
+let area = pi * radio**2;
 
 
+document.getElementById('long').innerHTML = long;
 document.getElementById('area').innerHTML = area;
 
 
