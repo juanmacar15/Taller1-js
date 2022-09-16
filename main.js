c351c8f2a579fbe23fe21767961f8b7103f98b5b
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-15 20:05:07 -0500

    ejercicio 2

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-let n1 = parseInt(prompt('Ingresar numero 1'));
-let n2 = parseInt(prompt('Ingresar numero 2'));
+let n1 = parseInt(prompt('Ingrese el lado 1'));
+let n2 = parseInt(prompt('Ingrese el lado 2'));
 
-let suma = n1 + n2;
-let resta = n1 - n2;
-let mult = n1 * n2;
-let div = n1 / n2;
+let rect = n1 * n2;
 
-document.getElementById('suma').innerHTML = suma;
-document.getElementById('resta').innerHTML = resta;
-document.getElementById('mult').innerHTML = mult;
-document.getElementById('div').innerHTML = div;
+
+document.getElementById('rect').innerHTML = rect;
+
 
