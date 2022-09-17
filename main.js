401a500c0547666bb2ae735c2a72146b47e7d16e
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-17 07:50:50 -0500

    ejercicio 19

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-alert('sueldo empleado ')
+alert('promedio estudiante ')
+alert('porfavor ingresar las notas de 1 a 5 ')
 
-let pagoH = 15300;
+let exa1 = parseFloat(prompt('ingrese la nota del primer examen'));
+let exa2 = parseFloat(prompt('ingrese la nota del segundo examen'));
+let exa3 = parseFloat(prompt('ingrese la nota del tercer examen'));
+let exa4 = parseFloat(prompt('ingrese la nota del cuarto examen'));
 
-let nombre = (prompt('ingrese su nombre'));
-let horas = parseFloat(prompt('ingrese las horas trabajadas'));
+if (exa1<=5 && exa2<=5 && exa3<=5 && exa4<=5){
 
-let pago = horas * pagoH;
+    let pro=(exa1+exa2+exa3+exa4)/4;
 
-document.getElementById('nombre').innerHTML = nombre;
-document.getElementById('res').innerHTML = pago + ' pesos';
+    document.getElementById('nota').innerHTML = pro;
+
+}
+else{
+    alert('porfavor ingrese las notas de 1 a 5')
+}
+
 
 
 
