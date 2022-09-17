1d6270d12ec384c111b8a7954ff40f758fc61e58
juanmacar15 <jdmadrid19@misena.edu.co>
2022-09-17 07:20:43 -0500

    ejercicio 17

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-alert('promedio de materia ')
+alert('IVA de producto ')
 
-let par1 = parseFloat(prompt('nota del primer parcial'));
-let par2 = parseFloat(prompt('nota del segundo parcial'));
-let par3 = parseFloat(prompt('nota del tercer parcial'));
-let exa = parseFloat(prompt('nota el examen final'));
-let tra = parseFloat(prompt('nota del trabajo final'));
+let pro = parseFloat(prompt('valor del producto'));
+let iva = parseFloat(prompt('digite el iva sin el singo %'));
 
+let iva2=iva/100;
+let iva3=pro*iva2;
 
-let a=((par1+par2+par3)/3)*0.55;
-let b=exa*0.30;
-let c=tra*0.15;
+let total = pro + iva3;
 
-let cal=a+b+c;
-
-document.getElementById('res').innerHTML = cal;
+document.getElementById('iva').innerHTML = iva3 + ' pesos';
+document.getElementById('res').innerHTML = total + ' pesos';
 
 
 
